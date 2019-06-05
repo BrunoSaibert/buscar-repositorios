d1cab06ef67785975d2f07ab7707f8c53b7ac316
BrunoSaibert <brunosaibert@gmail.com>
2019-06-05 09:47:41 -0300

    Ajustes para enviar para produção

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: ['@babel/polyfill', './src/main.js'],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '/public')
+    path: path.resolve(__dirname, './public')
   },
   devServer: {
     contentBase: __dirname + '/public'
