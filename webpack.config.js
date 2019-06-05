4ddcab8c094a32b564f33eb89e7ee321a5b56805
BrunoSaibert <brunosaibert@gmail.com>
2019-06-04 21:42:16 -0300

    Adicionar estrutura inicial

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['@babel/polyfill', './src/main.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '/public')
+  },
+  devServer: {
+    contentBase: __dirname + '/public'
+  },
+module: {
+    rules: [
+      {
+        test: /\.js/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
